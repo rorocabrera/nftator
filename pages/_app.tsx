3f996cacfc7080af35c6a390f7e625c218b96359
rorocabrera <rorocabrera@gmail.com>
2022-11-22 21:26:29 -0400

    first commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,29 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app"
+import { ChakraProvider } from "@chakra-ui/react"
+import WalletContextProvider from '../components/WalletContextProvider'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import { extendTheme } from "@chakra-ui/react"
+
+const colors = {
+  background: "#1F1F1F",
+  accent: "#833BBE",
+  bodyText: "rgba(255, 255, 255, 0.75)",
 }
+
+const theme = extendTheme({ colors })
+
+function MyApp({ Component, pageProps }: AppProps) {
+//these Wraps are something that I wanto to understand, specially in comparison to my flutter knowledge
+
+
+  return (
+    <ChakraProvider theme={theme}>
+      <WalletContextProvider>
+		<Component {...pageProps} />
+	</WalletContextProvider>
+
+    </ChakraProvider>
+  )
+}
+
+export default MyApp
